32be4bdfd12271f6ed01adc3cccb9f69e134a33a
yakupkalayci <ykpklyc18@gmail.com>
2022-07-17 11:32:39 +0300

    fix undefined error

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -95,7 +95,7 @@ export async function getStaticPaths() {
   });
 
   const data = await response.json();
-  const paths = data?.items.map((item) => ({ params: { id: String(item.id) } }));
+  const paths = await data?.items.map((item) => ({ params: { id: String(item.id) } }));
 
   return {
     paths,
